61c4d1d2cc00f057be88014ad732fac23e55b67e
Cristian Mira <cristian_mira@epam.com>
2022-03-30 11:45:33 -0500

    RestAssure examples

--- src/test/java/ApiServices.java
+++ src/test/java/ApiServices.java
@@ -1,9 +1,9 @@
 import entity.Customer;
+import io.restassured.response.Response;
 import org.json.JSONObject;
 
 import java.util.concurrent.TimeUnit;
 
-import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 
 public class ApiServices {
@@ -11,18 +11,75 @@ public class ApiServices {
     public static void main(String[] args) {
 
         ApiServices apiServices = new ApiServices();
-        apiServices.callRestGetServices();
-        apiServices.callRestGetServicesWithQueryParams();
-        apiServices.getResponseServiceTime();
+        //apiServices.callRestGetServices();
+        //apiServices.callRestPostService();
+        apiServices.callRestPutService();
+
+   //     apiServices.callRestGetServicesWithQueryParams();
+   //     apiServices.getResponseServiceTime();
 
        // apiServices.GetJsonObjectFromObjet(new Customer());
     }
 
     public void callRestGetServices(){
-        given().when().
-                get("http://demo.guru99.com/V4/sinkministatement.php?CUSTOMER_ID=68195&PASSWORD=1234!&Account_No=1")
-                .then().log().all();
+      given().when().
+                get("https://dog.ceo/api/breeds/image/random")
+                .then().log().body();
     }
+
+    public void callRestPostService(){
+
+        String requestBody =  "{\n" +
+                "  \"title\": \"foo\",\n" +
+                "  \"body\": \"bar\",\n" +
+                "  \"userId\": \"1\" \n}";
+
+      Response response =  given().
+                header("Content-type", "application/json")
+                .and()
+                .body(requestBody)
+                .when()
+                .post("https://jsonplaceholder.typicode.com/posts")
+                .then().extract().response();
+        System.out.println(response.statusCode());
+
+
+
+    }
+
+    public void callRestPutService(){
+
+        String requestBody =  "{\n" +
+                "  \"title\": \"foo\",\n" +
+                "  \"body\": \"baz\",\n" +
+                "  \"userId\": \"1\",\n" +
+                "  \"id\": \"1\" \n}";
+
+       given().
+                header("Content-type", "application/json")
+                .and()
+                .body(requestBody)
+                .when()
+                .put("https://jsonplaceholder.typicode.com/posts/1")
+                .then().assertThat().statusCode(200)
+               .log().all();
+       ;
+
+
+    }
+    public void callRestDeleteService(){
+        Response response =  given().
+                header("Content-type", "application/json")
+                .when()
+                .delete("https://jsonplaceholder.typicode.com/posts/1")
+                .then().extract().response();
+        System.out.println(response.statusCode());
+
+
+
+    }
+
+
 
     public void callRestGetServicesWithQueryParams(){
         given().
